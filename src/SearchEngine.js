40da4fb8a53fbfc9f92ad8dc50dd04277716114e
jujuPlanchon <juliafoplanchon@gmail.com>
2022-02-23 15:35:54 +0100

    Created weather App in react

---
+++ src/SearchEngine.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "89edb83e45300d77a9a1a3bef122681f";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  function newCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={newCity} />
+      <button type="submit">Search</button>
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul className="list">
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}Km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
+export default SearchEngine;
---
+++ src/Weather.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import SearchEngine from "./SearchEngine";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Geneva",
+    temperature: 10,
+    date: "Mon, 13 Dec 2021",
+    hour: "06:00",
+    description: "Sunny",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 10,
+    wind: 20,
+  };
+  return (
+    <div className="App">
+      <div className="weather-app">
+        <div className="overview">
+          <h1>{weatherData.city} </h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li>{weatherData.hour}</li>
+            <li>{weatherData.description}</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span className="float-left">
+                <strong>{weatherData.temperature}</strong>
+                <span className="units">
+                  <a
+                    className="celsius"
+                    href="https://https://en.wikipedia.org/wiki/Celsius"
+                    target="_blank"
+                    rel="noreferrer"
+                    id="celsius-link"
+                  >
+                    °C
+                  </a>{" "}
+                  |
+                  <a
+                    className="fahr"
+                    href="https://https://en.wikipedia.org/wiki/Fahrenheit"
+                    target="_blank"
+                    rel="noreferrer"
+                    id="fahrenheit-link"
+                  >
+                    °F
+                  </a>
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>
+                Humidity: <span>{weatherData.humidity}</span>%
+              </li>
+              <li>
+                Wind: <span>{weatherData.wind}</span>km/h
+              </li>
+            </ul>
+          </div>
+        </div>
+        <SearchEngine />
+        <p>Open Source at </p>
+      </div>
+    </div>
+  );
+}
